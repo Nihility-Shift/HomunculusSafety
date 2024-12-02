0743b05216c2c4498d136b4197f5917e6a038fed
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-02 00:09:25 -0800

    Updated Documentation

--- HomunculusSafety/MyPluginInfo.cs
+++ HomunculusSafety/MyPluginInfo.cs
@@ -7,8 +7,8 @@ namespace HomunculusSafety
         public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
         public const string PLUGIN_NAME = "HomunculusSafety";
         public const string USERS_PLUGIN_NAME = "Homunculus Safety";
-        public const string PLUGIN_VERSION = "0.0.2";
-        public const string PLUGIN_DESCRIPTION = "Recreates the homunculus if it gets lost. Host side.";
+        public const string PLUGIN_VERSION = "0.0.3";
+        public const string PLUGIN_DESCRIPTION = "Recreates the homunculus if it gets lost.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107, Dragon";
         public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Homunculus_Safety";
